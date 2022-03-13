e1e0216ef710192a69aa4cc249e5c26f180382b2
udim12 <ehud6401@gmail.com>
2022-03-13 20:44:57 +0200

    changed the printing statement

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int i=0; i<=10; i++){
-            System.out.println("hello "+i);
+            System.out.println("hello Ehud "+i);
         }
     }
 }
