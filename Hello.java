27427d90cc1f36731df54fe445bf5959381b7426
udim12 <ehud6401@gmail.com>
2022-03-13 20:48:16 +0200

    changed the printing statement again

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int i=0; i<=10; i++){
-            System.out.println("hello Ehud run4 "+i);
+            System.out.println("hello Ehud run5 "+i);
         }
     }
 }
